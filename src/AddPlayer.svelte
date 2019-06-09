1c3563e1a0cc98c39b17719abb381fbb3923668a
paxarpp <pavelpp1974@gmail.com>
2019-06-09 21:25:11 +0300

    error create player

--- src/AddPlayer.svelte
+++ src/AddPlayer.svelte
@@ -3,6 +3,7 @@
 
   const dispatcher = createEventDispatcher();
 
+  let error = false;
   let player = {
     name: '',
     points: 0,
@@ -10,16 +11,23 @@
 
   const onSubmit = (e) => {
     e.preventDefault();
+    if (!player.name.trim()) {
+      error = true;
+      return;
+    }
+    error = false;
     dispatcher('addplayer', player);
     player = {
       name: '',
       points: 0,
     };
   };
+  const resetError = () => error = false;
 </script>
 
 <style>
   form {
+    position: relative;
     padding: 10px;
     width: 500px;
     display: flex;
@@ -29,10 +37,20 @@
     background-color: lightblue;
     cursor: pointer;
   }
+  .error {
+    color: red;
+    position: absolute;
+    top: 2rem;
+    left: 20px;
+    font-size: 0.7rem;
+  }
 </style>
 
 <form on:submit={onSubmit}>
-  <input type="text" placeholder="Player name" bind:value={player.name}>
+{#if error}
+  <label class="error">Имя не может быть пустым</label>
+{/if}
+  <input type="text" placeholder="Player name" bind:value={player.name} on:focus={resetError}>
   <input type="number" placeholder="Player points" bind:value={player.points}>
   <input type="submit" value="Add Player">
 </form>
